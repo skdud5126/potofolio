989369ca7f073117fc8d3b7f5f7867c34418ec87
skdud5126 <91711479+skdud5126@users.noreply.github.com>
2021-10-10 23:59:26 +0900

    Add files via upload

--- js/slide_banner.js
+++ js/slide_banner.js
@@ -39,3 +39,35 @@ $(function(){ // $(document).ready(function(){ });
         current = i;
     }
 });
+
+// 메인메뉴 버튼
+
+$(function(){
+    var menu = $('#main_mn > li');
+    var contents = $('#contents > div');
+    
+    menu.click(function(event){
+       event.preventDefault();
+       
+       var tg = $(this);
+       var i = tg.index();
+       
+       var section = contents.eq(i);
+       var tt = section.offset().top;
+       
+       $('html, body').stop().animate({scrollTop:tt});
+    });
+    
+     $(window).scroll(function(){
+       var sct = $(window).scrollTop();
+      
+       contents.each(function(){
+         var tg = $(this);
+         var i = tg.index();
+          if(tg.offset().top <= sct){
+              menu.removeClass('on');
+              menu.eq(i).addClass('on');
+          }
+      });
+    });
+});
